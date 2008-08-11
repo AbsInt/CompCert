9679d3e613960068031ae1a01af68602420a76a2
xleroy <xleroy@fca1b0fc-160b-0410-b1d3-a4f43f01ea2e>
2008-08-11 13:59:49 +0000

    New file
    git-svn-id: https://yquem.inria.fr/compcert/svn/compcert/trunk@713 fca1b0fc-160b-0410-b1d3-a4f43f01ea2e

---
+++ caml/Clflags.ml
@@ -0,0 +1,25 @@
+(* *********************************************************************)
+(*                                                                     *)
+(*              The Compcert verified compiler                         *)
+(*                                                                     *)
+(*          Xavier Leroy, INRIA Paris-Rocquencourt                     *)
+(*                                                                     *)
+(*  Copyright Institut National de Recherche en Informatique et en     *)
+(*  Automatique.  All rights reserved.  This file is distributed       *)
+(*  under the terms of the INRIA Non-Commercial License Agreement.     *)
+(*                                                                     *)
+(* *********************************************************************)
+
+(* Command-line flags *)
+
+let prepro_options = ref ([]: string list)
+let linker_options = ref ([]: string list)
+let exe_name = ref "a.out"
+let option_flonglong = ref false
+let option_fmadd = ref false
+let option_dclight = ref false
+let option_dasm = ref false
+let option_E = ref false
+let option_S = ref false
+let option_c = ref false
+let option_v = ref false
