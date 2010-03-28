939d3effd3f63e5a51d6bf0a6cf36e6ab82aac70
xleroy <xleroy@fca1b0fc-160b-0410-b1d3-a4f43f01ea2e>
2010-03-28 11:23:02 +0000

    Extra volatile test
    git-svn-id: https://yquem.inria.fr/compcert/svn/compcert/trunk@1297 fca1b0fc-160b-0410-b1d3-a4f43f01ea2e

---
+++ test/regression/volatile2.c
@@ -0,0 +1,30 @@
+/* Checking that __builtin_volatile_xxx functions are correctly compiled */
+
+#include <stdio.h>
+
+#define TEST(msg,ty,x,v1,v2) \
+  *((volatile ty *) &x) = v1;                                      \
+  printf("%s 1: %s\n", msg, *((ty *) &x) == v1 ? "OK" : "FAILED"); \
+  *((ty *) &x) = v2;                                               \
+  printf("%s 2: %s\n", msg, *((volatile ty *) &x) == v2 ? "OK" : "FAILED");
+
+int main(int argc, char ** argv)
+{
+  signed char sc;
+  unsigned char uc;
+  signed short ss;
+  unsigned short us;
+  int i;
+  float f;
+  double d;
+
+  TEST("signed char", signed char, sc, 12, 34);
+  TEST("unsigned char", unsigned char, uc, 56, 78);
+  TEST("signed short", signed short, ss, 1234, 5678);
+  TEST("unsigned short", unsigned short, us, 1357, 2468);
+  TEST("int", int, i, 0x123456, 0x7890AB);
+  TEST("float", float, f, 0.5, 256.0);
+  TEST("double", double, d, 3.1415, 2.718);
+  return 0;
+}
+
