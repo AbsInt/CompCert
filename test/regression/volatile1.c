812e142ed14d95023da491f2bd31ab568a7e1351
xleroy <xleroy@fca1b0fc-160b-0410-b1d3-a4f43f01ea2e>
2010-09-04 10:14:21 +0000

    ++ on volatile not supported.
    git-svn-id: https://yquem.inria.fr/compcert/svn/compcert/trunk@1504 fca1b0fc-160b-0410-b1d3-a4f43f01ea2e

--- test/regression/volatile1.c
+++ test/regression/volatile1.c
@@ -2,8 +2,8 @@ volatile int v;
 
 int f1(void) { return v; }
 
-int f2(void) { return v++; }
+void f2(void) { v = 42; }
 
-int f3(void) {return v / v + 1 + v; }
+int f3(void) { return v / v + 1 + v; }
 
 void f4(void) { v; }
