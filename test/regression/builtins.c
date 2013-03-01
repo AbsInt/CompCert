d689d99bfeb5d12e3ed2eb3849b6a4b652577d4d
xleroy <xleroy@fca1b0fc-160b-0410-b1d3-a4f43f01ea2e>
2013-03-01 18:52:55 +0000

    Some builtins were renamed, updating
    git-svn-id: https://yquem.inria.fr/compcert/svn/compcert/trunk@2134 fca1b0fc-160b-0410-b1d3-a4f43f01ea2e

--- test/regression/builtins.c
+++ test/regression/builtins.c
@@ -55,11 +55,11 @@ int main(int argc, char ** argv)
 
 #endif
 
-  printf ("read_16_rev = %x\n", __builtin_read_int16_reversed(&s));
-  printf ("read_32_rev = %x\n", __builtin_read_int32_reversed(&y));
-  __builtin_write_int16_reversed(&s, 0x789A);
+  printf ("read_16_rev = %x\n", __builtin_read16_reversed(&s));
+  printf ("read_32_rev = %x\n", __builtin_read32_reversed(&y));
+  __builtin_write16_reversed(&s, 0x789A);
   printf ("after write_16_rev: %x\n", s);
-  __builtin_write_int32_reversed(&y, 0x12345678);
+  __builtin_write32_reversed(&y, 0x12345678);
   printf ("after write_32_rev: %x\n", y);
 
   return 0;
