c52566fff3d2cb7f41817f52ddaa0ffc7864c843
xleroy <xleroy@fca1b0fc-160b-0410-b1d3-a4f43f01ea2e>
2014-02-21 08:29:28 +0000

    PowerPC is big-endian, dammit.
    git-svn-id: https://yquem.inria.fr/compcert/svn/compcert/trunk@2412 fca1b0fc-160b-0410-b1d3-a4f43f01ea2e

--- powerpc/Archi.v
+++ powerpc/Archi.v
@@ -20,13 +20,13 @@ Require Import ZArith.
 Require Import Fappli_IEEE.
 Require Import Fappli_IEEE_bits.
 
-Definition big_endian := false.
+Definition big_endian := true.
 
 Notation align_int64 := 8%Z (only parsing).
 Notation align_float64 := 8%Z (only parsing).
 
 Program Definition default_pl : bool * nan_pl 53 := 
-  (true, nat_iter 51 xO xH).
+  (false, nat_iter 51 xO xH).
 
 Definition choose_binop_pl (s1: bool) (pl1: nan_pl 53) (s2: bool) (pl2: nan_pl 53) :=
   false.                        (**r always choose first NaN *)
