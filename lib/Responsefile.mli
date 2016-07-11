1ad10395dc17a4257d26e8a854cb98e7107ceff5
Bernhard Schommer <bernhardschommer@gmail.com>
2016-07-11 10:48:59 +0200

    Added function to write responsefiles.
    The arguments are written in the responsefile separated by whitespace.
    If the argument itself contains a whitespace it is quoted.
    Bug 18308

---
+++ lib/Responsefile.mli
@@ -0,0 +1,24 @@
+(* *********************************************************************)
+(*                                                                     *)
+(*              The Compcert verified compiler                         *)
+(*                                                                     *)
+(*          Xavier Leroy, INRIA Paris-Rocquencourt                     *)
+(*        Bernhard Schommer, AbsInt Angewandte Informatik GmbH         *)
+(*                                                                     *)
+(*  Copyright Institut National de Recherche en Informatique et en     *)
+(*  Automatique.  All rights reserved.  This file is distributed       *)
+(*  under the terms of the GNU General Public License as published by  *)
+(*  the Free Software Foundation, either version 2 of the License, or  *)
+(*  (at your option) any later version.  This file is also distributed *)
+(*  under the terms of the INRIA Non-Commercial License Agreement.     *)
+(*                                                                     *)
+(* *********************************************************************)
+
+
+val expand_responsefiles: string array -> string array
+  (** Expand responsefile arguments contained in the array and return the full
+      set of arguments. *)
+
+val write_responsefile: out_channel -> string array -> unit
+  (** Write the arguments on the out_channel. All arguments that contain
+      whitespaces are quoted. *)
