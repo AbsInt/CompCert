c2ff34c38c130df21ebae95806ef451a74e2ea6a
Bernhard Schommer <bernhardschommer@gmail.com>
2017-01-09 10:29:28 +0100

    Reverted mistaken change for endianess config. Bug 20521

--- driver/Configuration.ml
+++ driver/Configuration.ml
@@ -127,7 +127,11 @@ let arch =
   | v -> bad_config "arch" [v]
 let model = get_config_string "model"
 let abi = get_config_string "abi"
-let is_big_endian = get_bool_config "endianness"
+let is_big_endian =
+  match get_config_string "endianness" with
+  | "big" -> true
+  | "little" -> false
+  | v -> bad_config "endianness" [v]
 let system = get_config_string "system"
 let has_runtime_lib = get_bool_config "has_runtime_lib"
 let has_standard_headers = get_bool_config "has_standard_headers"
