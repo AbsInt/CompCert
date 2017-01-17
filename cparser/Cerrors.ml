e2919da34ac8105b22e116a6b687a6f544b05142
Bernhard Schommer <bernhardschommer@gmail.com>
2017-01-17 15:13:47 +0100

    Remove duplaceted relese. Bug  20681

--- cparser/Cerrors.ml
+++ cparser/Cerrors.ml
@@ -325,7 +325,7 @@ let raise_on_errors () =
 let crash exn =
   if Version.buildnr <> "" && Version.tag <> "" then begin
     let backtrace = Printexc.get_backtrace () in
-    eprintf "%tThis is CompCert, Release:%s, Build:%s, Tag:%s%t\n"
+    eprintf "%tThis is CompCert, %s, Build:%s, Tag:%s%t\n"
       bc Version.version Version.buildnr Version.tag rsc;
     eprintf "Backtrace (please include this in your support request):\n%s"
       backtrace;
