1b0f3229551f3f183cb53bde602541ddd3fa094e
chaomaer <1506174913@qq.com>
2019-03-09 16:52:08 +0800

    update the comment of the free operation
    I think there is a mistake in the comment. It should be `freeable` not `writable`

--- common/Memtype.v
+++ common/Memtype.v
@@ -104,7 +104,7 @@ Parameter alloc: forall (m: mem) (lo hi: Z), mem * block.
 
 (** [free m b lo hi] frees (deallocates) the range of offsets from [lo]
   included to [hi] excluded in block [b].  Returns the updated memory
-  state, or [None] if the freed addresses are not writable. *)
+  state, or [None] if the freed addresses are not freeable. *)
 Parameter free: forall (m: mem) (b: block) (lo hi: Z), option mem.
 
 (** [load chunk m b ofs] reads a memory quantity [chunk] from
