df24f45af3e156670a39b553ebbcaba7403f0af3
Xavier Leroy <xavier.leroy@college-de-france.fr>
2019-04-16 14:04:06 +0200

    Provide a default "select" operation for the RiscV port
    No `Osel` operation for this port, so `SelectOp.select` always
    returns None.

--- riscV/SelectOpproof.v
+++ riscV/SelectOpproof.v
@@ -873,6 +873,20 @@ Proof.
   red; intros. unfold floatofsingle. TrivialExists.
 Qed.
 
+Theorem eval_select:
+  forall le ty cond al vl a1 v1 a2 v2 a b,
+  select ty cond al a1 a2 = Some a ->
+  eval_exprlist ge sp e m le al vl ->
+  eval_expr ge sp e m le a1 v1 ->
+  eval_expr ge sp e m le a2 v2 ->
+  eval_condition cond vl m = Some b ->
+  exists v, 
+     eval_expr ge sp e m le a v
+  /\ Val.lessdef (Val.select (Some b) v1 v2 ty) v.
+Proof.
+  unfold select; intros; discriminate.
+Qed.
+
 Theorem eval_addressing:
   forall le chunk a v b ofs,
   eval_expr ge sp e m le a v ->
