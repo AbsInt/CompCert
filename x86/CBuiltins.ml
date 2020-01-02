2696f9b4a626229879248d7c97de252619a4e3b2
Bernhard Schommer <bernhardschommer@gmail.com>
2020-01-02 15:02:20 +0100

    Remove __builtin_nop from list of x86 builtins.

--- x86/CBuiltins.ml
+++ x86/CBuiltins.ml
@@ -73,9 +73,6 @@ let builtins = {
       (TVoid [], [TPtr(TInt(IUShort, []), []); TInt(IUShort, [])], false);
     "__builtin_write32_reversed",
       (TVoid [], [TPtr(TInt(IUInt, []), []); TInt(IUInt, [])], false);
-    (* no operation *)
-    "__builtin_nop",
-      (TVoid [], [], false);
   ]
 }
 
