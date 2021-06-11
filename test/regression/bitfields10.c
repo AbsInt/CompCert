6036c1b1ece329396ee8e37015c6b7e490cd765f
Xavier Leroy <xavier.leroy@college-de-france.fr>
2021-06-11 10:14:58 +0200

    Split bitfield initialization test in two
    bitfields9 is for static initialization
    bitfields10 is for dynamic initialization.
    
    Dynamic initialization currently produces run-time errors with the
    reference interpreter, so don't test bitfields10 in interpreted mode.

--- test/regression/bitfields10.c
+++ test/regression/bitfields10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/* Static initialization of bit-fields */
+/* Dynamic initialization of bit-fields */
+/* Known not to work with the reference interpreter */
 
 struct s {
   signed char a: 6;
@@ -39,32 +40,27 @@ void print_u_v(char * msg, union u p)
   printf("%s = { v = %u }\n", msg, p.v);
 }
 
-/* Global initialization */
-struct s glob_s = { -12, 1 };
-struct t glob_t = { 123, 2, -45 };
-union u glob_u_u = { -3 };
-union u glob_u_v = { .v = 6 };
-
-/* Local, static initialization */
-void f(void)
+/* Local, non-static initialization */
+void f(int x, int y, int z)
 {
-  static struct s loc_s = { -12, 1 };
-  static struct t loc_t = { 123, 2, -45 };
-  static union u loc_u_u = { -3 };
-  static union u loc_u_v = { .v = 6 };
+  struct s loc_s = { x, y };
+  struct t loc_t = { x, z, y };
+  union u loc_u_u = { .u = x };
+  union u loc_u_v = { .v = z };
   print_s("loc_s", loc_s);
   print_t("loc_t", loc_t);
   print_u_u("loc_u_u", loc_u_u);
   print_u_v("loc_u_v", loc_u_v);
+  print_s("compound_s", (struct s) { y, x });
+  print_t("compound_t", (struct t) { y, ~z, -x });
+  print_u_u("compound_u", (union u) { y });
 }
 
 int main()
 {
-  print_s("glob_s", glob_s);
-  print_t("glob_t", glob_t);
-  print_u_u("glob_u_u", glob_u_u);
-  print_u_v("glob_u_v", glob_u_v);
-  f();
+  f(11, 2, 3);
+  f(7, 50, 2);
   return 0;
 }
+
 
