4e12d0e8e2cbfd3e5215860824fd24c0aeec9c07
Xavier Leroy <xavier.leroy@college-de-france.fr>
2023-01-19 14:10:20 -0500

    Revised translation of bit-field declarations to CompCert C
    Should be equivalent to the current code, but easier to understand.

--- cfrontend/C2C.ml
+++ cfrontend/C2C.ml
@@ -445,7 +445,7 @@ let checkFunctionType env tres targs =
   | Some l -> List.iter (fun (id, ty) -> checkArgumentType env ty) l
   end
 
-let rec convertTyp env ?bitwidth t =
+let rec convertTyp env t =
   match t with
   | C.TVoid a -> Tvoid
   | C.TInt(ik, a) ->
@@ -476,21 +476,7 @@ let rec convertTyp env ?bitwidth t =
   | C.TUnion(id, a) ->
       Tunion(intern_string id.name, convertAttr a)
   | C.TEnum(id, a) ->
-      let ik =
-        match bitwidth with
-        | None -> Cutil.enum_ikind
-        | Some w ->
-            let info = Env.find_enum env id in
-            let representable sg =
-              List.for_all (fun (_, v, _) -> Cutil.int_representable v w sg)
-                           info.Env.ei_members in
-            if representable false then
-              Cutil.unsigned_ikind_of Cutil.enum_ikind
-            else if representable true then
-              Cutil.signed_ikind_of Cutil.enum_ikind
-            else
-              Cutil.enum_ikind in
-      convertIkind ik (convertAttr a)
+      convertIkind Cutil.enum_ikind (convertAttr a)
 
 and convertParams env = function
     | [] -> Tnil
@@ -525,13 +511,38 @@ let rec convertTypAnnotArgs env = function
       Tcons(convertTyp env (Cutil.unary_conversion env e1.etyp),
             convertTypAnnotArgs env el)
 
+(* Convert struct and union definitions *)
+
+let rec convertBitfieldTyp env name width t =
+  match t with
+  | C.TNamed _ ->
+      convertBitfieldTyp env name width (Cutil.unroll env t)
+  | C.TEnum(id, a) ->
+      let info = Env.find_enum env id in
+      let representable sg =
+        List.for_all (fun (_, v, _) -> Cutil.int_representable v width sg)
+                     info.Env.ei_members in
+      let ik =
+        if representable false then
+          Cutil.unsigned_ikind_of Cutil.enum_ikind
+        else if representable true then
+          Cutil.signed_ikind_of Cutil.enum_ikind
+        else
+          Cutil.enum_ikind
+        in
+      convertIkind ik (convertAttr a)
+  | _ ->
+      convertTyp env t
+
 let convertField env sid f =
-  let id = intern_string f.fld_name
-  and ty = convertTyp env ?bitwidth: f.fld_bitfield f.fld_typ in
+  let id = intern_string f.fld_name in
   Debug.set_member_atom ~str_id:sid f.fld_name ~fld_id:id;
   match f.fld_bitfield with
-  | None -> Member_plain(id, ty)
+  | None ->
+      let ty = convertTyp env f.fld_typ in
+      Member_plain(id, ty)
   | Some w ->
+      let ty = convertBitfieldTyp env f.fld_name w f.fld_typ in
       match ty with
       | Tint(sz, sg, attr) ->
           Member_bitfield(id, sz, sg, attr, Z.of_uint w, f.fld_name = "")
