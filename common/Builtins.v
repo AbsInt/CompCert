5eb840227cb37f9632260a65b338435e832c4f22
Xavier Leroy <xavier.leroy@college-de-france.fr>
2025-03-14 18:18:54 +0100

    More lemmas about known builtin functions

--- common/Builtins.v
+++ common/Builtins.v
@@ -42,6 +42,28 @@ Definition builtin_function_sem (b: builtin_function) : builtin_sem (sig_res (bu
   | BI_platform b => platform_builtin_sem b
   end.
 
+Lemma builtin_function_sem_inject: forall b vargs vres f vargs',
+  builtin_function_sem b vargs = Some vres ->
+  Val.inject_list f vargs vargs' ->
+  exists vres', builtin_function_sem b vargs' = Some vres' /\ Val.inject f vres vres'.
+Proof.
+  intros. exploit (bs_inject _ (builtin_function_sem b)); eauto.
+  unfold val_opt_inject; rewrite H; intro J.
+  destruct (builtin_function_sem b vargs') as [vres'|]; try contradiction.
+  exists vres'; auto.
+Qed.
+
+Lemma builtin_function_sem_lessdef: forall b vargs vres vargs',
+  builtin_function_sem b vargs = Some vres ->
+  Val.lessdef_list vargs vargs' ->
+  exists vres', builtin_function_sem b vargs' = Some vres' /\ Val.lessdef vres vres'.
+Proof.
+  intros. apply val_inject_list_lessdef in H0. 
+  exploit builtin_function_sem_inject; eauto.
+  intros (vres' & A & B). apply val_inject_lessdef in B.
+  exists vres'; auto.
+Qed.
+
 Definition lookup_builtin_function (name: string) (sg: signature) : option builtin_function :=
   match lookup_builtin standard_builtin_sig name sg standard_builtin_table with
   | Some b => Some (BI_standard b)
