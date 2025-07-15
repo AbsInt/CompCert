727a943b3b1271c98fc5a530e0903d4da634eef2
Xavier Leroy <xavier.leroy@college-de-france.fr>
2025-07-15 13:56:01 +0200

    x86-64: support IBT (control-flow integrity for indirect jumps)

--- driver/Clflags.ml
+++ driver/Clflags.ml
@@ -40,6 +40,7 @@ let option_mthumb = ref (Configuration.model = "armv7m")
 let option_Osize = ref false
 let option_finline = ref true
 let option_finline_functions_called_once = ref true
+let option_fcf_protection = ref false
 let option_dprepro = ref false
 let option_dparse = ref false
 let option_dcmedium = ref false
