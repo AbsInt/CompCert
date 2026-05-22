10d5b87b2debd35f0e8f3062a766fd3178c1b8e2
Xavier Leroy <xavier.leroy@college-de-france.fr>
2026-05-22 16:22:15 +0200

    AArch64: better use of store pair in function prologues
    Even when the stack frame is too big for a preindexed store pair,
    we can use a SP decrement followed by a store pair, saving one instruction.

--- aarch64/Asmexpand.ml
+++ aarch64/Asmexpand.ml
@@ -18,6 +18,7 @@ open Asmexpandaux
 open AST
 open Camlcoq
 module Ptrofs = Integers.Ptrofs
+module I64 = Integers.Int64
 
 exception Error of string
 
@@ -38,9 +39,6 @@ let expand_loadimm32 (dst: ireg) n =
 
 let expand_addimm64 (dst: iregsp) (src: iregsp) n =
   List.iter emit (Asmgen.addimm64 dst src n [])
-
-let expand_storeptr (src: ireg) (base: iregsp) ofs =
-  List.iter emit (Asmgen.indexed_memory_access (fun a -> Pstrx(src, a)) (Z.of_uint 8) base ofs [])
 
 (* Handling of varargs *)
 
@@ -501,21 +499,20 @@ let expand_instruction instr =
       end else begin
         current_function_stacksize := Z.to_int64 sz
       end;
+      assert (retaddrofs = Z.(add linkofs (of_uint 8)));
       let sz1 = Z.(sub sz linkofs) in
-      if retaddrofs = Z.(add linkofs (of_uint 8))
-      && Z.(le sz1 (of_uint 512)) then begin
-        emit (Pstp(X15, X30, ADpreincr(XSP, Z.neg sz1)));
-        emit (Pcfi_adjust sz1);
-        let sz2 = Z.(sub sz sz1) in
-        if Z.(gt sz2 zero) then begin
-          expand_addimm64 XSP XSP (Ptrofs.repr (Z.neg sz2));
-          emit (Pcfi_adjust sz2)
-        end
+      if Z.(le sz1 (of_uint 512)) then begin
+        emit (Pstp(X15, X30, ADpreincr(XSP, I64.repr (Z.neg sz1))));
+        emit (Pcfi_adjust sz1)
       end else begin
-        expand_addimm64 XSP XSP (Ptrofs.repr (Z.neg sz));
-        emit (Pcfi_adjust sz);
-        expand_storeptr X15 XSP linkofs;
-        expand_storeptr X30 XSP retaddrofs
+        expand_addimm64 XSP XSP (Ptrofs.repr (Z.neg sz1));
+        emit (Pcfi_adjust sz1);
+        emit (Pstp(X15, X30, ADimm(XSP, I64.zero)))
+      end;
+      let sz2 = Z.(sub sz sz1) in
+      if Z.(gt sz2 zero) then begin
+        expand_addimm64 XSP XSP (Ptrofs.repr (Z.neg sz2));
+        emit (Pcfi_adjust sz2)
       end
   | Pfreeframe (sz, ofs) ->
       expand_addimm64 XSP XSP (coqint_of_camlint64 !current_function_stacksize)
